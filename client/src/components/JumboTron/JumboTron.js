761adc947e8fd9446a330cc1c335b5ccca2b1ade
Lisa Vinson <Lrv0828@gmail.com>
2018-04-02 22:18:47 -0400

    added basic structure of homepage with react-bootstrap: navbar, jumbotron, left-col text, right-col tabs for sign-in and sign-up

---
+++ client/src/components/JumboTron/JumboTron.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import "./JumboTron.css";
+
+const JumboTron = () => (
+<Jumbotron className="customJumbotron">
+    <h2>When is the last time you learned something new? </h2>
+</Jumbotron>
+);
+
+export default JumboTron;
--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,31 +1,69 @@
 import React from 'react';
-import "./style.css";
+import './style.css';
+import NavBar from '../../components/NavBar';
+import JumboTron from '../../components/JumboTron';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+// import { myTabs } from '../../components/Tab';
+import { Tabs, Tab } from 'react-bootstrap';
+
 // import { Link } from 'react-router-dom';
 import SignIn from '../../components/SignIn';
 import SignUp from '../../components/SignUp';
 
 const HomePage = (props) => {
-	
-		return (
-			<div>
 
-				<SignIn
-					handleChange={props.handleChange}
-					handleSubmit={props.handleSubmit}
-					username={props.username}
-					password={props.password} 
-					/>
-				<SignUp
-					handleChange={props.handleChange}
-					handleSubmit={props.handleSubmit}
-					username={props.username}
-					password={props.password}
-					/>
+	return (
+		<div>
+			<NavBar />
+			<JumboTron />
+			<Grid>
+				<Row>
+					<Col xs={12} sm={6}>
 
-			</div>
-		)
-	
+						<div>
+							<h3>Some Information About the Website</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod mi id massa cursus, eget fringilla neque ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut quis convallis lorem, eget interdum ex. Integer fermentum scelerisque consequat.
+							</p>
+							<br />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod mi id massa cursus, eget fringilla neque ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut quis convallis lorem, eget interdum ex. Integer fermentum scelerisque consequat.
+							</p>
+							<br />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod mi id massa cursus, eget fringilla neque ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut quis convallis lorem, eget interdum ex. Integer fermentum scelerisque consequat.
+							</p><br />
+						</div>
+					</Col>
+					<Col xs={12} sm={6}>
+						<Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
 
+							<Tab eventKey={1} title="Sign Up">
+							<SignUp
+									handleChange={props.handleChange}
+									handleSubmit={props.handleSubmit}
+									username={props.username}
+									password={props.password}
+								/>
+
+							</Tab>
+							<Tab eventKey={2} title="Sign In">
+								<SignIn
+									handleChange={props.handleChange}
+									handleSubmit={props.handleSubmit}
+									username={props.username}
+									password={props.password}
+								/>
+							</Tab>
+						</Tabs>
+					</Col>
+				</Row>
+			</Grid>
+
+		</div>
+	)
 };
 
 export default HomePage;
